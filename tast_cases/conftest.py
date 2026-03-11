45035e4f17066696a503f60c943a6d356539128a
Ibrahim shake shuvo <ibrahimshake100@gmail.com>
2026-03-12 01:01:46 +0900

    forget to add headless mode

--- tast_cases/conftest.py
+++ tast_cases/conftest.py
@@ -9,7 +9,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 @pytest.fixture
 def setup():
     chrome_option = Options()
-    # chrome_option.add_argument("--headless")
+    chrome_option.add_argument("--headless")
     chrome_option.add_argument("--no-sandbox")
     chrome_option.add_argument('--disable-gpu')
     chrome_option.add_argument('--window-size=1920,1080')
